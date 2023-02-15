9b28755a3051531af9c47b53e3aa7edcc10dcac0
HamZik <b1y9mn@gmail.com>
2023-02-15 22:12:17 +0900

    feat: Font Awesome에서 가져온 삭제 아이콘 삽입

--- src/components/Todo/TodoItem/TodoItem.js
+++ src/components/Todo/TodoItem/TodoItem.js
@@ -1,16 +1,20 @@
 // import React from 'react'
 import styles from './TodoItem.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 const TodoItem = (props) => {
   const onDelete = (id) => props.onDelete(id);
 
   return (
-    <div className={styles.items}>
+    <div>
       {props.item.map((todo) => (
-        <div 
-          className={styles.item} 
-          key={todo.id} 
-          onClick={() => onDelete(todo.id)}>{todo.text}
+        <div className={styles.textAndDeteteBtn} key={todo.id}>
+          <div 
+            className={styles.text} 
+            onClick={() => onDelete(todo.id)}>{todo.text}
+          </div>
+          <button><FontAwesomeIcon icon={faTrashCan} size="2x"color="white"/></button>
         </div>
       ))}
     </div>
