0f175c5df0c6f3056c19af79b6dcaf7cca7213ce
HamZik <b1y9mn@gmail.com>
2023-02-17 20:38:42 +0900

    feat: GoalTitle 입력값 줄바뀌게 수정

--- src/components/Goal/GoalTitle/GoalTitle.js
+++ src/components/Goal/GoalTitle/GoalTitle.js
@@ -1,9 +1,11 @@
-import styles from './GoalTitle.module.css';
+import styles from "./GoalTitle.module.css";
 
 const GoalTitle = (props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>{props.input}</div>
+      {props.input.map((goal) => (
+        <div className={styles.title} key={goal}>{goal}</div>
+      ))}
     </div>
   );
 };
