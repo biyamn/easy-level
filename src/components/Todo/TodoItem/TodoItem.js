8e9c92907b01a58afd0229ba9544ac83ec4a70c9
HamZik <b1y9mn@gmail.com>
2023-02-18 16:21:40 +0900

    feat: label로 체크박스 커스텀 디자인

--- src/components/Todo/TodoItem/TodoItem.js
+++ src/components/Todo/TodoItem/TodoItem.js
@@ -1,40 +1,52 @@
-
-import styles from './TodoItem.module.css';
+import styles from "./TodoItem.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
-import { useState } from 'react';
+import { useState } from "react";
 
 const TodoItem = (props) => {
   const [isDeleteClicked, setIsDeleteClicked] = useState(false);
 
   const openDelete = () => {
-    setIsDeleteClicked(true)
-  }
-  
+    setIsDeleteClicked(true);
+  };
+
   const cancelDelete = () => {
-    setIsDeleteClicked(false)
-  }
+    setIsDeleteClicked(false);
+  };
 
   const onDelete = (id) => {
     props.onDelete(id);
-  }
-  
+  };
+
   return (
-      <div className={styles.textAndDeteteBtn}>
-        <div className={styles.checkboxAndText}>
-          <input type="checkbox"></input>
-          <div className={styles.text}>{props.todo.text}</div>
-        </div>
-        {isDeleteClicked ? 
-          <div>
-            <button className={styles.delteOkIcon} onClick={() => onDelete(props.todo.id)}><FontAwesomeIcon icon={faCheck} size="2x" color="white"/></button>
-            <button className={styles.delteNoIcon} onClick={cancelDelete}><FontAwesomeIcon icon={faXmark} size="2x" color="white"/></button>
-          </div>
-          : <button className={styles.delteIcon} onClick={openDelete}><FontAwesomeIcon icon={faTrashCan} size="2x" color="white"/></button> 
-        }
+    <div className={styles.textAndDeteteBtn}>
+      <div className={styles.checkboxAndText}>
+        <label>
+          <input type="checkbox" />
+          <div />
+        </label>
+        <div className={styles.text}>{props.todo.text}</div>
       </div>
+      {isDeleteClicked ? (
+        <div>
+          <button
+            className={styles.delteOkIcon}
+            onClick={() => onDelete(props.todo.id)}
+          >
+            <FontAwesomeIcon icon={faCheck} size="2x" color="white" />
+          </button>
+          <button className={styles.delteNoIcon} onClick={cancelDelete}>
+            <FontAwesomeIcon icon={faXmark} size="2x" color="white" />
+          </button>
+        </div>
+      ) : (
+        <button className={styles.delteIcon} onClick={openDelete}>
+          <FontAwesomeIcon icon={faTrashCan} size="2x" color="white" />
+        </button>
+      )}
+    </div>
   );
 };
 
