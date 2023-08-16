b8eac4a70473e08962cbdd5fa22abc84a8cc72b1
HamZik <b1y9mn@gmail.com>
2023-08-17 00:37:53 +0900

    chore: eslint 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,11 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
