115088db9d12a38acb3dc0fc6b0cb7fad82b3601
HamZik <b1y9mn@gmail.com>
2023-08-25 03:35:56 +0900

    feat: Firebase 연동 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,22 @@ import React from "react";
 import styles from "./App.module.css";
 import Todo from "./components/Todo/Todo/Todo";
 import Goal from "./components/Goal/Goal/Goal";
+import { key } from "../key";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = key;
+console.log("firebaseConfig", firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const App = () => {
   return (
