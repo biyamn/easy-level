7ae3075460c3ff09917a1d131f948eb1a10aef54
biyam <b1y9mn@gmail.com>
2023-11-16 16:39:45 +0900

    feat: select form 스타일링

--- src/components/Goal/GoalInput.jsx
+++ src/components/Goal/GoalInput.jsx
@@ -4,14 +4,20 @@ import styles from './GoalInput.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
-const options = [
-  { value: '기술면접' },
-  { value: '인성면접' },
-  { value: '포트폴리오' },
-  { value: '기타' },
-];
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const GoalInput = ({ onGoalSubmit }) => {
+  const options = [
+    { value: '기술면접' },
+    { value: '인성면접' },
+    { value: '포트폴리오' },
+    { value: '기타' },
+  ];
+
   const [enteredGoal, setEnteredGoal] = useState('');
   const [isValid, setIsValid] = useState(true);
   const [selectedOption, setSelectedOption] = useState(options[0].value);
@@ -35,24 +41,47 @@ const GoalInput = ({ onGoalSubmit }) => {
     setSelectedOption(options[0].value);
   };
 
-  const Select = () => {
+  const SelectType = () => {
     const handleChange = (e) => {
       setSelectedOption(e.target.value);
       console.log(e.target.value);
     };
 
     return (
-      <select onChange={handleChange} value={selectedOption}>
-        {options.map((option) => (
-          <option key={option.value}>{option.value}</option>
-        ))}
-      </select>
+      <Box sx={{ minWidth: 120, mr: '10px', height: '50px' }}>
+        <FormControl
+          variant="filled"
+          sx={{
+            bgcolor: '#fff',
+            borderRadius: '6px',
+            marginBottom: '100px',
+            color: 'white',
+          }}
+          fullWidth
+        >
+          <InputLabel id="demo-simple-select-label">면접 분류</InputLabel>
+          <Select
+            sx={{ height: '50px' }}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedOption}
+            label="Age"
+            onChange={handleChange}
+          >
+            {options.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.value}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
     );
   };
   return (
     <div className={styles.container}>
       <form className={styles.form} onSubmit={handleGoalSubmit}>
-        <Select />
+        <SelectType />
         <input
           className={
             !isValid ? `${styles.input} ${styles.invalid}` : `${styles.input}`
@@ -60,7 +89,7 @@ const GoalInput = ({ onGoalSubmit }) => {
           type="text"
           value={enteredGoal}
           onChange={handleGoalChange}
-          placeholder="면접 종류를 추가해 주세요."
+          placeholder="상세 면접 종류를 추가해 주세요."
         />
         <button className={styles.button} type="submit">
           <FontAwesomeIcon icon={faPlus} color="#1a202c" />
