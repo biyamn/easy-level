5d5de21f1b88004e85967505f28628b09185c1b9
biyam <b1y9mn@gmail.com>
2023-11-29 02:23:27 +0900

    feat: 모바일 뷰포트에서 면접 종류 carousel 적용

--- src/components/Goal/GoalItems.jsx
+++ src/components/Goal/GoalItems.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './GoalItems.module.css';
 import GoalItem from './GoalItem';
 import { Carousel } from 'react-bootstrap';
+import styled from 'styled-components';
 
 const GoalItems = ({
   goals,
@@ -21,9 +22,9 @@ const GoalItems = ({
   const ABLED = '#a8dcfa';
   let backgroundColor = DISABLED;
 
-  return (
-    <div className={styles.container}>
-      <Carousel fade>
+  const mobileView = (
+    <Mobile className={styles.container}>
+      <Carousel slide={false}>
         {goals.map((goal) => {
           if (goal.id === selectedGoal) {
             backgroundColor = ABLED;
@@ -46,8 +47,52 @@ const GoalItems = ({
           );
         })}
       </Carousel>
-    </div>
+    </Mobile>
+  );
+
+  const desktopView = (
+    <Desktop className={styles.container}>
+      {goals.map((goal) => {
+        if (goal.id === selectedGoal) {
+          backgroundColor = ABLED;
+        } else {
+          backgroundColor = DISABLED;
+        }
+        console.log(goal.id);
+        return (
+          <GoalItem
+            key={goal.id}
+            goal={goal}
+            todos={todos}
+            onGoalDelete={handleGoalDelete}
+            onGoalEdit={handleGoalEdit}
+            onSelectGoal={handleSelectedGoal}
+            backgroundColor={backgroundColor}
+            selectedGoal={selectedGoal}
+          />
+        );
+      })}
+    </Desktop>
+  );
+
+  return (
+    <>
+      {desktopView}
+      {mobileView}
+    </>
   );
 };
+
+const Desktop = styled.div`
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const Mobile = styled.div`
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
 
 export default GoalItems;
